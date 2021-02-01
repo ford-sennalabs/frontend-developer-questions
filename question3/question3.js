f405a62c36fee063b94d9f8c1eb16c0c332d9fa2
nrc2539 <narongchai.b2539@gmail.com>
2021-02-01 20:48:23 +0700

    upload question3

---
+++ question3/question3.js
@@ -0,0 +1,40 @@
+const testCase = [
+	[1,1,1,2,1,1],
+	[0,0, 0.55, 0, 0],
+	[1, 0, 0, 0, 0],
+	[0, 1, 0, 0, 0]
+]
+
+const findUniq = (arr) => {
+	let valuesAlreadySeen = []
+	for(let i=0; i< arr.length; i++){
+		let value = arr[i]
+    if (valuesAlreadySeen.indexOf(value) !== -1) {
+      continue
+    }
+    valuesAlreadySeen.push(value)
+	}
+	
+	for(let index=0; index < valuesAlreadySeen.length; index++){
+		let count_duplicate = howManyDuplicate(valuesAlreadySeen[index], arr)
+		if(count_duplicate === 1){
+			return valuesAlreadySeen[index]
+		}
+	}
+	return valuesAlreadySeen
+}
+
+const howManyDuplicate = (value, arr) => {
+	let count = 0
+	for(let i=0; i< arr.length;i++){
+		if(arr[i] === value) {
+			count = count + 1
+		}
+	}
+	return count
+}
+
+testCase.forEach(currentCase => {
+	let result = findUniq(currentCase)
+	console.log(`case [${currentCase}] return ${result}`)
+});
